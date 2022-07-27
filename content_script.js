500813d07a41dac08b3c1136f683b469a1af56eb
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2022-07-27 14:20:05 +0530

    Set visibility of menu on everymouseup

--- content_script.js
+++ content_script.js
@@ -1,4 +1,4 @@
-window.addEventListener('contextmenu', () => {
+window.addEventListener('mouseup', (e) => {
     const selectedText = window.getSelection().toString()
     chrome.runtime.sendMessage({selection: selectedText});
 });
